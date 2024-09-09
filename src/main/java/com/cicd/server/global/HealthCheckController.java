2ea91b4c0f02098fc53fb68132ef73f457f1083c
jiwonniy <jiwoni0707@gmail.com>
2024-09-09 15:06:01 +0900

    chore: 필요없는 import문 삭제

--- src/main/java/com/cicd/server/global/HealthCheckController.java
+++ src/main/java/com/cicd/server/global/HealthCheckController.java
@@ -1,5 +1,4 @@
 package com.cicd.server.global;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
